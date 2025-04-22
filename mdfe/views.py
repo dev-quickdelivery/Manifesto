8efa2c427efcac2ccf6361dfa8a3954f407ec226
lgluiz1 <legalhints@gmail.com>
2025-04-22 17:41:33 -0300

    "addition:"new function view manifest"

--- mdfe/views.py
+++ mdfe/views.py
@@ -15,3 +15,31 @@ def finalizar_transporte(request, manifesto_id):
     manifesto.save()
     finalizar_transporte_api(manifesto.id)  # Simulação da API
     return redirect('admin:index')
+
+def iniciar_manifesto(request, manifesto_id):
+    manifesto = get_object_or_404(Manifesto, id=manifesto_id)
+    manifesto.status = 'Em_transporte'  # ou o valor que você usa
+    manifesto.save()
+    messages.success(request, "Manifesto iniciado com sucesso.")
+    return redirect('painel')  # redirecione para a lista de manifestos
+
+
+@login_required
+def ver_manifesto(request, manifesto_id):
+    # Buscar a instância de Motorista associada ao usuário logado
+    motorista = get_object_or_404(Motorista, user=request.user)
+
+    # Agora buscamos o manifesto, verificando se ele pertence ao motorista correto
+    manifesto = Manifesto.objects.all().get(id=manifesto_id, motorista=motorista)
+
+    if manifesto.status != 'Em_transporte':
+        messages.warning(request, "Este manifesto ainda não está em transporte.")
+        return redirect('painel')
+
+    return render(request, 'manifesto/ver_manifesto.html', {
+        'manifesto': manifesto,
+        'notas': manifesto.notas.all()  # ou o nome correto do related_name se estiver definido
+    })
+
+
+
