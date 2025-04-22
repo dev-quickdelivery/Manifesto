43fe9af22592a5e5a99eda164888c6e2d661a1be
lgluiz1 <legalhints@gmail.com>
2025-04-22 17:30:27 -0300

    "addition:"Addition of a new function to the admin panel"

--- mdfe/admin.py
+++ mdfe/admin.py
@@ -25,6 +25,18 @@ class ManifestoAdmin(admin.ModelAdmin):
     acoes.allow_tags = True
 
 
-admin.site.register(Manifesto, ManifestoAdmin)
+# mdfe/admin.py
+from django.contrib import admin
+from .models import Manifesto
+
+@admin.register(Manifesto)
+class ManifestoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'motorista', 'status')
+    
+    @admin.action(description='Iniciar transporte selecionado(s)')
+    def iniciar_transporte(self, request, queryset):
+        updated = queryset.update(status='Em_transporte')
+        self.message_user(request, f'{updated} manifesto(s) iniciado(s) com sucesso.')
+
 admin.site.register(Nota)
 admin.site.register(Canhoto)
