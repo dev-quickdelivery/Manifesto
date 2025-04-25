6af133677c9afe43248ea39426c5650303e9e5e8
lgluiz1 <legalhints@gmail.com>
2025-04-25 17:49:19 -0300

    cleanup: cleaning api requests to the admin panel

--- mdfe/admin.py
+++ mdfe/admin.py
@@ -1,33 +1,9 @@
 from django.contrib import admin
-from .models import  Manifesto, Nota, Canhoto
+from .models import  Manifesto , Transportadora
 from django.urls import path
 from django.utils.html import format_html
-from .views import iniciar_transporte, finalizar_transporte
+
 from django.urls import reverse
-
-"""
-
-class ManifestoAdmin(admin.ModelAdmin):
-    list_display = ['id', 'status', 'criado_em', 'acoes']
-
-    def get_urls(self):
-        urls = super().get_urls()
-        custom_urls = [
-            path('<int:manifesto_id>/iniciar/', self.admin_site.admin_view(iniciar_transporte), name='iniciar_transporte'),
-            path('<int:manifesto_id>/finalizar/', self.admin_site.admin_view(finalizar_transporte), name='finalizar_transporte'),
-        ]
-        return custom_urls + urls
-
-    def acoes(self, obj):
-        return format_html(
-            '<a class="button" href="{}">Iniciar</a> <a class="button" href="{}">Finalizar</a>',
-            f"/admin/app/manifesto/{obj.id}/iniciar/",
-            f"/admin/app/manifesto/{obj.id}/finalizar/",
-        )
-    acoes.short_description = 'Ações'
-    acoes.allow_tags = True
-
-   """
 
 
 # mdfe/admin.py
@@ -36,12 +12,13 @@ from .models import Manifesto
 
 @admin.register(Manifesto)
 class ManifestoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'motorista', 'status')
+    list_display = ('numero_rota', 'motorista', 'status')
     
     @admin.action(description='Iniciar transporte selecionado(s)')
     def iniciar_transporte(self, request, queryset):
         updated = queryset.update(status='Em_transporte')
         self.message_user(request, f'{updated} manifesto(s) iniciado(s) com sucesso.')
 
-admin.site.register(Nota)
-admin.site.register(Canhoto)
+
+
+admin.site.register(Transportadora)
