2dd2ec93691723c7c79c4a25ba315e9016dead59
lgluiz1 <legalhints@gmail.com>
2025-04-25 17:55:18 -0300

    removal: removal of unused functions and useless variables

--- motorista/views.py
+++ motorista/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Motorista
+from .utils import registrar_motoristas
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
-
 from django.http import JsonResponse
 from motorista.models import Motorista
-from mdfe.models import Manifesto, Nota, Canhoto
+from mdfe.models import Manifesto
 from django.contrib.auth.decorators import login_required
+from mdfe.utils import criar_manifesto
 
 
 def verificar_cpf(request):
+    
     if request.method == "POST":
         cpf = request.POST.get("cpf")
         motorista = Motorista.objects.filter(cpf=cpf).first()
@@ -26,19 +28,17 @@ def verificar_cpf(request):
 
 @login_required
 def painel_view(request):
+    criar_manifesto()
     manifesto = Manifesto.objects.all()
-    nota = Nota.objects.all()
     motorista = Motorista.objects.get(user=request.user)
 
     #pega todos os manifestos do motorista logado
     manifesto = manifesto.filter(motorista=motorista)
 
-    #pega todas as notas vinculado ao manifesto do motorista logado
-    nota = nota.filter(manifesto__motorista=motorista)
+
 
     context = {
-        'manifesto': manifesto,
-        'nota': nota,
+        'manifesto': manifesto,        
         'motorista': motorista
     }
 
